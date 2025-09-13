a9debb4f693293e7079df68808de3d6f60a12cab
Scott McHenry <scottmchenry@Scotts-MacBook-Air.local>
2025-09-13 19:30:45 -0400

    More bandit fixes

--- backend/main.py
+++ backend/main.py
@@ -128,4 +128,4 @@ app.include_router(api_router)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="info")  # nosec B104
